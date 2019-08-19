5ebf38bac3961cedb53dcf8faa6ceff062887e6b
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-19 16:59:29 +0100

    add controllers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,10 @@
+class BookingsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # get 'bookings/show'
+  # get 'bookings/create'
+  # get 'bookings/destroy'
+  # get 'kitties/index'
+  # get 'kitties/show'
+  # get 'kitties/create'
+  # get 'kitties/update'
+  # get 'kitties/edit'
+  # get 'kitties/destroy'
+  # get 'kitties/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
