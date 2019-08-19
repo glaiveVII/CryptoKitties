50504c31feceae5d2186088c3276d7d38fe22826
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-19 18:36:57 +0100

    fix bug

--- app/models/kitty.rb
+++ app/models/kitty.rb
@@ -8,13 +8,11 @@ class Kitty < ApplicationRecord
   validates :public_key, presence: true
   validates :price, presence: true
   validates :user_owner_id, presence: true
-  validates :available, presence: true, default: true
+  validates :available, presence: true
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :public_key, presence: true
   validates :price, presence: true
   validates :user_owner_id, presence: true
-
-
 end
