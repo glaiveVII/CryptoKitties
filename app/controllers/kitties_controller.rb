43be9058eae472791b5bee5099ba47b1a5608fb1
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-19 17:40:45 +0100

    add method to controller to make it work, may have mistype issues.... #endofday

--- app/controllers/kitties_controller.rb
+++ app/controllers/kitties_controller.rb
@@ -1,22 +1,61 @@
 class KittiesController < ApplicationController
+  before_action :set_kitty, only: [:show, :edit, :update, :destroy]
+
   def index
+    @kitties = Kitty.all
   end
 
   def show
   end
 
-  def create
-  end
-
-  def update
+  def new
+    @kitty = Kitty.new
   end
 
   def edit
   end
 
-  def destroy
+  def create
+    @kitty = Kitty.new(kitty_params)
+
+    respond_to do |format|
+      if @kitty.save
+        format.html { redirect_to @kitty, notice: 'Kitty was successfully created.' }
+        format.json { render :show, status: :created, public_key: @kitty }
+      else
+        format.html { render :new }
+        format.json { render json: @kitty.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def new
+  def update
+    respond_to do |format|
+      if @kitty.update(kitty_params)
+        format.html { redirect_to @kitty, notice: 'Kitty was successfully updated.' }
+        format.json { render :show, status: :ok, public_key: @kitty }
+      else
+        format.html { render :edit }
+        format.json { render json: @kitty.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @kitty.destroy
+    respond_to do |format|
+      format.html { redirect_to kitties_url, notice: 'Kitty was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_kitty
+    @kitty = Kitty.find(params[:id])
+  end
+
+  def kitty_params
+    params.require(:kitty).permit(:first_name, :last_name)
   end
 end
