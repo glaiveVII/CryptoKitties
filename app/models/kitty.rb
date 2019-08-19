4e44644d54a1a2cb19c59ce763738c7f010aed9a
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-19 15:47:37 +0100

    mistake added code in master

--- app/models/kitty.rb
+++ app/models/kitty.rb
@@ -1,3 +1,9 @@
 class Kitty < ApplicationRecord
   belongs_to :user
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :public_key, presence: true
+  validates :price, presence: true
+  validates :user_owner_id, presence: true
+
 end
