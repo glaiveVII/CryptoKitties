6cfc3fca2f280541d5126caed998de91a2cad856
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-21 14:36:56 +0100

    all booking working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :kitties do
-    resources :bookings, only: %i[new create]
+    resources :bookings, only: %i[index new create]
   end
-  resources :bookings, only: %i[index show destroy]
+  resources :bookings, only: %i[show destroy]
 end
