ec553b942698e02be48415e1b87dad05261d05dd
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-21 09:32:25 +0100

    add geocode and other stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-
-import { initMapbox } from '../plugins/init_mapbox';
-
-initMapbox();
