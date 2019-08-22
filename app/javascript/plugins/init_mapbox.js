b266b0e06de30ab9513d5316686135ff4a6f52fe
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-22 12:10:54 +0100

    try doing the sweet alert

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -30,18 +30,6 @@ const initMapbox = () => {
 
 // sweet alert code
 
-import swal from 'sweetalert';
-
-const initSweetalert = (selector, options = {}) => {
-  const swalButton = document.querySelector(selector);
-  if (swalButton) { // protect other pages
-    swalButton.addEventListener('click', () => {
-      swal(options);
-    });
-  }
-};
-
-export { initSweetalert };
 
 
 export { initMapbox };
