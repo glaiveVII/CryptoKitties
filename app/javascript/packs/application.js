c0789de631daac72c2ab527593784789782cbbd7
Julien Pelegri <pelegrijulien@hotmail.fr>
2019-08-22 12:17:39 +0100

    sweet alert done nedd to fix a small bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import "../plugins/flatpickr"
 import { initMapbox } from '../plugins/init_mapbox';
 import { initSweetalert } from '../plugins/init_sweetalert';
 
-initSweetalert('#sweet-alert-demo', {
+initSweetalert('.sweet-alert-demo', {
   title: "A nice alert",
   text: "This is a great alert, isn't it?",
   icon: "success"
